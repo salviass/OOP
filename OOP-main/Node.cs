57a7fc3bc4f3b940e74f1154c2c50ba8e19f7732
salviass <75370102+salviass@users.noreply.github.com>
2020-12-08 10:42:42 +0200

    Add files via upload

---
+++ OOP-main/Node.cs
@@ -0,0 +1,11 @@
+﻿using System;
+
+public class Node<T>
+{
+    public Node(T data)
+    {
+        Data = data;
+    }
+    public T Data { get; set; }
+    public Node<T> Next { get; set; }
+}
---
+++ OOP-main/Program.cs
@@ -0,0 +1,32 @@
+﻿using SimpleAlgorithmsApp;
+using System;
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            LinkedList<string> linkedList = new LinkedList<string>();
+// добавление элементов
+linkedList.Add("Tom");
+linkedList.Add("Alice");
+linkedList.Add("Bob");
+linkedList.Add("Sam");
+ 
+// выводим элементы
+foreach(var item in linkedList)
+{
+    Console.WriteLine(item);
+}
+// удаляем элемент
+linkedList.Remove("Alice");
+foreach (var item in linkedList)
+{
+    Console.WriteLine(item);
+}
+// проверяем наличие элемента
+bool isPresent = linkedList.Contains("Sam");
+Console.WriteLine(isPresent == true ? "Sam присутствует" : "Sam отсутствует");
+ 
+// добавляем элемент в начало            
+linkedList.AppendFirst("Bill");
+        }
+    }
